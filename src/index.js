2a7de40ad0d6cc6b1eb5adaf2436e72cbb450ba4
Terry Chen <yenhsuan.terry@gmail.com>
2020-01-13 23:45:49 -0800

    fix eslint error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux';
 import configureStore from './store/store';
 
 const store = configureStore();
