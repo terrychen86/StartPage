f58f0bec70f521a1fc7342a9bb6ed10799b1b46b
Terry Chen <yenhsuan.terry@gmail.com>
2020-01-20 16:30:22 -0800

    Add default icon

--- src/utils/fetchIcon.js
+++ src/utils/fetchIcon.js
@@ -3,11 +3,13 @@
 import amazon from 'icons/amazon_1220386.png';
 import apple from 'icons/apple_1220384.png';
 import bing from 'icons/bing_1220382.png';
+import defaultIcon from 'icons/default_3761814.png';
 
 const ICONS = {
   amazon,
   apple,
   bing,
+  defaultIcon,
 };
 
 const fetchIcon = (name: $Keys<typeof ICONS>): string => ICONS[name];
